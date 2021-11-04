987d5f635068ac05eb5536b3f9e9615f3ced8dee
Csutoras Peter <csutiperec@gmail.com>
2021-11-04 20:04:26 +0100

    Added Invoice Component

--- components/Invoice.tsx
+++ components/Invoice.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { View } from 'react-native'
+import Button from './Button'
 
 const Invoice = () => {
     return (
-        <>
-            
-        </>
+        <View>
+            <View>
+                {/*List of Invoice Items*/}
+            </View>
+            <View>
+                <Button text='Add Item'/>
+            </View>
+            <View>
+                <Button text='Calculate'/>
+            </View>
+        </View>
     )
 }
 
